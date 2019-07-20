a727fdaa576da21a5a6748131c42430df33dd42d
alintulu <adelinalintu@gmail.com>
2019-07-20 13:38:10 +0300

    add unity project

---
+++ Game/Assets/Scripts/HandMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandMovement : MonoBehaviour
+{
+    CharacterController characterController;
+
+    public float speed = 6.0f;
+    public string inputh;
+    public string inputv;
+
+
+    private Vector2 moveDirection = Vector2.zero;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        moveDirection = new Vector2(Input.GetAxis(inputh), Input.GetAxis(inputv));
+        moveDirection *= speed;
+
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+}
+
---
+++ Game/Assets/Scripts/LoadSceneManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneManager : MonoBehaviour
+{
+
+    public AudioClip onClick;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        //audioSource = GetComponent<AudioSource>();
+    }
+
+    public void LoadMain()
+    {
+        //audioSource.PlayOneShot(onClick, 0.7F);
+        //(SceneManager.LoadScene("MainScene", LoadSceneMode.Single);
+        Debug.Log("Load Main");
+    }
+
+    public void LoadCredits()
+    {
+        //audioSource.PlayOneShot(onClick, 0.7F);
+        //SceneManager.LoadScene("CreditScene", LoadSceneMode.Single);
+         Debug.Log("Load Credits");
+    }
+
+    public void Exit()
+    {
+        //Application.Quit();
+    }
+}
